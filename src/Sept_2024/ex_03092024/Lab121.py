9f11c73cd2ed704bf548eb50a64361781541e183
Dhir <dhir.kr3@gmail.com>
2024-09-06 14:50:46 -0400

    Class Work for Sept 3rd, 2024

--- src/Sept_2024/ex_03092024/Lab121.py
+++ src/Sept_2024/ex_03092024/Lab121.py
@@ -5,16 +5,17 @@
 class Car:
     model = None
     name = None
-    password = 123
+    password = 123 # This variable should be private in nature
 
+    # def __init__(self):
+    #   print("DC")
     def __init__(self):
-        self.password = "pramod"
+     self.password = "dhir"
 
     def change_password(self):
         print(self.password)
 
 
-
 object_ref = Car()
 print(object_ref.password)
-object_ref.change_password()
+object_ref.change_password() # used by outside the class --> obj reference
