3e0646dde67897810cf6fc6cbbcb3a206ab6a666
Dhir <dhir.kr3@gmail.com>
2024-10-16 12:38:54 -0400

    Class Work for Oct 10th, 2024

---
+++ src/Oct_2024/test_Lab189.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv
+class Test_CRUD():
+
+    def test_update_1(self):
+        with open('src/Oct_2024/userdata.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print(row[0], row[1])
+    def test_update_2(self):
+        df = pd.read_csv('src/Oct_2024/userdata.csv')
+        print(df)
--- src/Sept_2024/ex_12092024/Lab167.py
+++ src/Sept_2024/ex_12092024/Lab167.py
@@ -1,2 +1,11 @@
+import calendar
+
 def greeting(name):
     print("Hello, " + name)
+
+year=2024
+month=1
+print(calendar.prcal(year))
+print("*******************************")
+print(calendar.prmonth(year,month))
+
